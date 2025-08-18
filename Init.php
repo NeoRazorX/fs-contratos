be63851e8a397055e56c069104b60c0b1635924f
Jose <jose@goltratec.com>
2025-08-18 09:39:38 +0200

    Ahora initClass viene de FacturaScripts\Core\Template\InitClass;

--- Init.php
+++ Init.php
@@ -1,14 +1,23 @@
 <?php
 namespace FacturaScripts\Plugins\Contratos;
 
-class Init extends \FacturaScripts\Core\Base\InitClass
+use FacturaScripts\Core\Template\InitClass;
+
+class Init extends InitClass
 {
-    public function init() {
+    public function init(): void
+    {
         $this->loadExtension(new Extension\Controller\EditCliente());
         // se ejecutara cada vez que carga FacturaScripts (si este plugin está activado).
     }
 
-    public function update() {
+    public function update(): void
+    {
         // se ejecutara cada vez que se instala o actualiza el plugin.
+    }
+
+    public function uninstall(): void
+    {
+        // TODO: Implement uninstall() method.
     }
 }
